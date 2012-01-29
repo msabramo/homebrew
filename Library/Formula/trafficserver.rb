63591c27fd9822305e36d03fe7aff20e2254b0d6
Marc Abramowitz <marc@marc-abramowitz.com>
2012-01-29 08:20:26 -0800

    Formula: trafficserver - Use brewed OpenSSL since Apple deprecated system OpenSSL in Lion

--- Library/Formula/trafficserver.rb
+++ Library/Formula/trafficserver.rb
@@ -6,10 +6,13 @@ class Trafficserver < Formula
   md5 '0f8e5ce658d28511001c6585d1e1813a'
 
   depends_on 'pcre'
+  depends_on 'openssl'
 
   def install
     ENV.enable_warnings
-    system "./configure", "--prefix=#{prefix}", "--with-user=#{ENV['USER']}", "--with-group=admin"
+    system "./configure", "--prefix=#{prefix}", "--with-user=#{ENV['USER']}", "--with-group=admin",
+           "CPPFLAGS=-I#{prefix}/Cellar/openssl/0.9.8r/include",
+           "LDFLAGS=-L#{prefix}/Cellar/openssl/0.9.8r/lib"
     system "make install"
   end
 
